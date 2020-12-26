bf1b3a9f5e926140727d9b604ab335607acb4edc
David McDanel <54825816+davemcdanel@users.noreply.github.com>
2020-12-26 12:17:21 -0600

    Uploaded main project files.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Smoker_Web
+
+A web based client for ProcessControlServer using controller cgi.
+
+A live version is running at...
+https://dave.mcdanel.gitea.kc0bpo.net/smoker_web/
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+		This is the PHP index file.  It is not currently used. Please load <a href="index.html">index.html</a>
+		<?php
+		// put your code here
+		?>
+    </body>
+</html>
